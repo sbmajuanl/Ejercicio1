1c0871848d1c651354d4928529c593b4d6a67e59
Brilly Majuan <sbmajuanl@gmail.com>
2017-03-05 18:06:33 -0500

    Refactorizando el codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,11 @@
-function EnviarDatos(){
+function enviardatos(){
 var nombre=document.getElementById("nombre");
 var apellido=document.getElementById("apellido");
 var dni=document.getElementById("dni");
 var direccion=document.getElementById("direccion");
 
 enviar.innerHTML =(
-
-  "<br>"+ "- Nombre : "+ nombre.value +
-  "</br>"+ "- Apellido : " + apellido.value +
-  "</br>"+ "- DNI : " + dni.value +
-  "</br>"+ "- Direccion : " + direccion.value);
-
-
+  "<div><ul><li> Nombre : "+ nombre.value+"</li><li> Apellido : " + apellido.value +
+          "</li><li> DNI : " + dni.value +"</li><li> Direccion : " + direccion.value+
+          "</li></ul><div>");
 }
